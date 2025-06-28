44aa8813a680c30849d8b17330f8dcea9336d985
hiuuvb <xyliganka17405@gmail.com>
2025-06-28 22:42:55 +0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 const { copyDeal, copyTasks, copyActivities } = require('./bitrix_deal_task_transfer');
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => logger.info(`🚀 Сервер запущен на порту ${PORT}`));
